223a3fa8dc5c243fb5cec49fba53d6f4e6659dc8
AaronACChen <AaronChen@cmail.carleton.ca>
2014-12-07 23:56:20 -0500

    SHRINK OUTPUT SPACE

--- A5Chain.c
+++ A5Chain.c
@@ -1,5 +1,7 @@
 #include "A5.h"
 
+
+
 A5State chainReduce(byte *outA){
 	unsigned long long stateB = 0;
 	A5State state;
@@ -9,26 +11,17 @@ A5State chainReduce(byte *outA){
 		stateB = stateB << 8;
 	}
 	stateB |= outA[7];
-	
-	//for (i=0; i<8; i++) printf("%02X", outA[i]);
-	//printf("\nWHOLE STATE: %16llX\n", stateB);
-	//stateB &= 0xFFFFFFFF00000000;
-	//printf("%016llX\n", stateB);
-	//printf("\n");
 	state.R1 = stateB>>45;
-	//printf("%016llX\n", state.R1);
 	state.R2 = stateB>>23;
 	state.R2&=SIZE2;
-	//printf("%016llX\n", state.R2);
 	state.R3 = stateB;
 	state.R3&=SIZE3;
-	//printf("%016llX\n", state.R3);
 	return state;
 }
 
 int done(byte outA[15]){
 	int done = 0,i;
-	for (i=12;i<=14;i++){
+	for (i=5;i<=7;i++){
 			done |= outA[i];
 	}
 	if (done == 0) return 1;
@@ -37,11 +30,17 @@ int done(byte outA[15]){
 
 void chain(A5State intialState, byte outA[15]){
 	long long count = 0;
+	int z;
 	A5State state = intialState;
 	A5SetState(&state);
 	A5QRun(outA);
 	while (!done(outA)){
 			count++;
+			if (count == 10000){
+				printf("\nKEYSTREAM:");
+				for (z=0; z<15; z++) printf("%02X", outA[z]);
+				printf("\n");
+			}
 			/*
 			if ((count&1048575)==1048575){
 				printf(" %d \n",count);
@@ -55,6 +54,27 @@ void chain(A5State intialState, byte outA[15]){
 			A5100Clock();
 			A5QRun(outA);
 	}
+	printf("\n%d \n",count);
+}
+
+void tableCreate(){
+	int i,z;
+	A5State state;
+	byte a[15];byte b[15];
+	byte key[8] = {0x12, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+	for (i = 0; i < 10; i++){
+		key[0]=i;
+		state=chainReduce(key);
+		chain(state,a);
+		printf("%u %u %u \n",state.R1,state.R2,state.R3);
+		for (z=0; z<8; z++) printf("%02X", a[z]);
+		printf("\n");
+	}
+}
+
+void tableSearch(){
+	
+
 }
 
 int main(){
@@ -67,14 +87,7 @@ int main(){
 	A5Run(a,b);
 	A5GetState(&state);
 		
-	for (i = 0; i < 10; i++){
-		key[0]=i;
-		state=chainReduce(key);
-		chain(state,a);
-		printf("%u %u %u \n",state.R1,state.R2,state.R3);
-		for (z=0; z<15; z++) printf("%02X", a[z]);
-		printf("\n");
-	}
+	table();
 	
 	//A5Setup(key,frame);
 		
