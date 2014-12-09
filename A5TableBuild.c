c080e340f2713510496d2f985e81c7669ea8474f
AaronACChen <AaronChen@cmail.carleton.ca>
2014-12-09 12:03:22 -0500

    restructure

--- A5TableBuild.c
+++ A5TableBuild.c
@@ -20,8 +20,15 @@ void tableCreate(int chains, int end){
 	}
 }
 
-int main(){
-	int i,z;
+int main(int argc, char *argv[]){
+	int i,z,chains,dp;
+	
+	if (argc < 3) {
+       fprintf(stderr,"usage %s chains distinguishedPointStart\n", argv[0]);
+       exit(0);
+    }
+	chains = atoi(argv[2]);
+	dp = atoi(argv[2]);
 	/*
 	byte key[8] = {0x12, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
 	reg frame = 0x134;
@@ -33,7 +40,7 @@ int main(){
 	A5GetState(&state);
 	printf("HEYHEYHEY\n");
 	*/
-	tableCreate(10,6);
+	tableCreate(chains,dp);
 	//tableSearch();
 	//A5Setup(key,frame);
 	
